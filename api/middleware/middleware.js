0011ba1b3b88b5051a990aba6674682a086972bf
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-03-10 17:16:17 -0600

    MVP

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,48 @@
+const Users = require('../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`${req.method} to ${req.url} - ${new Date().toISOString()}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+const validateUserId = async (req, res, next) => {
+  try{
+    const user = await Users.getById(req.params.id)
+    if(!user) {
+      res.status(404).json({ message: "user not found" })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch(err){
+    next(err)
+  }
 }
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+const validateUser = (req, res, next) => {
+  if(!req.body){
+    res.status(400).json({ message: "missing user data"})
+  } else if(!req.body.name){
+    res.status(400).json({ message: "missing required name field" })
+  } else {
+    next() // continue if body is ok!
+  }
 }
 
-function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+const validatePost = (req, res, next) => {
+ if(!req.body){
+   res.status(400).json({ message: "missing post data" })
+ } else if(!req.body.text) {
+   res.status(400).json({ message: "missing required text field" })
+ } else {
+   next()
+ }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
